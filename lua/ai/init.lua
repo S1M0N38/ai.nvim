4a0344e16dc8f4e1e9db5700f202af80b7479bb2
S1M0N38 <bertolottosimone@gmail.com>
2024-01-17 20:06:24 +0100

    Remove template examples and add modules

--- lua/ai/init.lua
+++ lua/ai/init.lua
@@ -16,45 +16,18 @@
 ---
 ---@brief ]]
 
----@class PluginNameModule
----@field setup function: setup the plugin
----@field main function: calculate the max or min of two numbers and round the result if specified by options
-local ai = {}
+local chat = require("ai.chat")
+local ai = {
+  chat_completion = chat.chat_completion,
+  chat_completion_stream = chat.chat_completion_stream,
+  -- TODO: add more endpoints
+}
 
 --- Setup the plugin
 ---@param options Config: config table
 ---@eval { ['description'] = require('ai.config').__format_keys() }
 ai.setup = function(options)
   require("ai.config").__setup(options)
-end
-
----Print the result of the comparison
----@param a number: first number
----@param b number: second number
----@param func string: "max" or "min"
----@param result number: result
-ai.print = function(a, b, func, result)
-  local s = "The " .. func .. " of " .. a .. " and " .. b .. " is " .. result
-  if require("ai.config").options.round then
-    s = s .. " (rounded)"
-  end
-  print(s)
-end
-
---- Calcululate the max or min of two numbers and round the result if specified by options
----@param a number: first number
----@param b number: second number
----@param func string: "max" or "min"
----@return number: result
-ai.main = function(a, b, func)
-  local options = require("ai.config").options
-  local mymath = require("ai.math")
-  local result = mymath[func](a, b)
-  if options.round then
-    result = mymath.round(result)
-  end
-  ai.print(a, b, func, result)
-  return result
 end
 
 return ai
